49182e17702067cc2787799e91a8ab8e8caaa165
薛海新 <xuehaixin@xuehaixindemacbook-pro.local>
2019-07-30 18:12:20 +0800

    Initial Commit

---
+++ Photon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photon
+//
+//  Created by 薛海新 on 2019/7/30.
+//  Copyright © 2019 薛海新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photon/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Photon
+//
+//  Created by 薛海新 on 2019/7/30.
+//  Copyright © 2019 薛海新. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
